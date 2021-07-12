2eb7dd2eac33b8bf8a22335d88c5562040d338cf
Ndifreke Anthony Ekim <ndyekim@gmail.com>
2021-07-12 12:22:50 +0100

    Add easy deploy microserver

--- server/demo.ts
+++ server/demo.ts
@@ -1,2 +1,13 @@
-import { demo } from './index'
-demo()
+import { demo } from './geoquery'
+// demo()
+
+// const util = require('./graphql/util');
+// const dotenv = require('dotenv');
+
+// const startBrowser = util.startBrowser;
+// dotenv.config();
+// import microServer from './microServer'
+
+
+// microServer({ port: 8080, callback: startBrowser })
+demo(8091)
---
+++ server/microServer.ts
@@ -0,0 +1,60 @@
+import { createServer } from 'http'
+const { schemaStructure } = require('./graphql/app');
+const { readFileSync } = require('fs');
+const dotenv = require('dotenv');
+import geoquery from './geoquery';
+dotenv.config();
+
+const getHTML = function () {
+    const htmlTemplate = readFileSync(__dirname + '/index.html');
+    const html = htmlTemplate.toString().replace('#graphQLschema', schemaStructure);
+    return html;
+};
+
+const handleGetRequest = function (req, res) {
+    res.end(getHTML());
+};
+
+const handlePostRequest = function (req, res) {
+    req.on('data', async (data) => {
+        console.log('POST:\n', data.toString());
+        const result = await geoquery(data.toString());
+        res.end(JSON.stringify(result, null, 2));
+    });
+};
+
+type MicroServerArgType = {
+    port: number
+    callback?: (arg: any) => void
+}
+
+/**
+ * Create A NodeJs server that servers the application
+ * @param param0 
+ */
+const microServer = function ({ port, callback }: MicroServerArgType) {
+    const PORT = port || process.env.PORT || 8080
+    const app = createServer();
+
+    app.on('request', async function (req, res) {
+
+        switch (req.method) {
+            case 'GET':
+                handleGetRequest(req, res);
+                break;
+
+            case 'POST':
+                handlePostRequest(req, res);
+                break;
+        }
+    });
+
+    app.listen(PORT);
+    console.log(`Listening on`, PORT, `http://localhost:${PORT}`);
+    callback?.({ port: PORT });
+};
+
+export default microServer
+// module.exports = runDemo;
+// export   {}
+
